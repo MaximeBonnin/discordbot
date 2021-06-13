3470d59669935a2e2315d502445b9d5e652ed086
MaximeBonnin <76616229+MaximeBonnin@users.noreply.github.com>
2021-06-13 22:09:25 +0200

    Update item finder
    Changed the website that is used for scraping. I hate html tables.

--- item_finder.py
+++ item_finder.py
@@ -1,69 +1,56 @@
-
 import requests
 from bs4 import BeautifulSoup
-# item finder for discord bot
 
-# format class:subclass
 
-# items -> https://www.aidedd.org/dnd/om.php?vo=alchemy-jug
-# should really use this https://5e.tools/items/alchemy-jug-dmg.html
-
-categories = {
-    "item": "om",
-    "spell": "sorts",
-    "monster": "monstres",
-    "feat": "feats",
-    "invocation": "eldritch-invocations"
-}
-
-def find_item(user_input="monster:ogre"):
-    # user_input = input("item: \n>>> ")
-
-    raw_input = user_input.lower().replace(" ", "-")
-    search_cat, search_term = raw_input.split(":")
-    # print(search_cat, search_term)
-
-    URL = f'https://www.aidedd.org/dnd/{categories[search_cat]}.php?vo={search_term}'
-    # print(URL)
-    site = requests.get(URL)
-    soup = BeautifulSoup(site.content, 'html.parser')
-
+def find_img(item):  # checks other site for img
     try:
-
+        URL = f'https://www.aidedd.org/dnd/om.php?vo={item}'
+        site = requests.get(URL)
+        soup = BeautifulSoup(site.content, 'html.parser')
         content = soup.find(class_='bloc')
+        img_found = content.find(class_="picture").find("amp-img")["src"]
+        return img_found
 
-        itemName = content.find("h1").text
-        # print(itemName)
-
-        itemRarity = content.find("em").text
-        # print(itemRarity)
-
-        itemDescription = content.find(class_="description").text
-
-        itemSource = content.find(class_="source").text
-        itemSource = itemSource.split("› ")[1]
-
-        found = {
-            "Name": itemName,
-            "Info": itemRarity,
-            "Source": itemSource,
-            "Description": itemDescription,
-            "Image": "No image"
-        }
-
-        try:
-            itemImg = content.find(class_="picture").find("amp-img")["src"]
-            # itemImg = f"https://www.aidedd.org/dnd/images-om/{search_term}.jpg"
-            found["Image"] = itemImg
-        except AttributeError:
-            print("No image found.")
-
-        # print(found)
-
-        return found
     except AttributeError:
-        print("Oops, no item found here...")
+        print("No image found.")
         return False
 
 
-find_item()
+def find_item_easy(term):  # checks main source for item info
+    item_type, item = term.split(":")
+    if item_type == "item":
+        item_type = "wondrous-items"
+    url2 = f"http://dnd5e.wikidot.com/{item_type}:{item}"
+
+    site = requests.get(url2)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    content = soup.find(id="page-content")
+
+    x = content
+    output = ""
+    while x.find_next().text != "":
+        if x.find_next().text.strip("\n") not in output:
+            y = x.find_next().text.strip("\n")
+            output = output + y + "\n"
+
+        x = x.find_next()
+
+    if f"The page {term} you want to access does not exist." in output:
+        print("Not found.")
+        return False
+    else:
+        title = ""   # format title
+        for i in item.split("-"):
+            title = title + i.capitalize() + " "
+
+        if find_img(item):   # attach img to top of output if one exists
+            output = f"{find_img(item)}\n{output}"
+        output = f"**{title}:**\n{output}"  # attach title to top of output
+
+    if len(output) > 2000:  # make output fit for discord character limit
+        msg = []
+        for i in range(0, len(output), 2000):
+            line = output[i:i+2000]
+            msg.append(line)
+        return msg
+    return [output]
